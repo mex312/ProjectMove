fa67aea3d1c4d1a8e926224fa998283f30ae50e3
mex312 <mex3122020@gmail.com>
2025-02-17 03:17:29 +0300

    rust added

--- godot/new_script.gd
+++ godot/new_script.gd
@@ -3,7 +3,9 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var a = Main.new()
+	
+	print(a.sqrt(4))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use godot::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod main_scene;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+struct ProjectMove;
+
+#[gdextension]
+unsafe impl ExtensionLibrary for ProjectMove {}
